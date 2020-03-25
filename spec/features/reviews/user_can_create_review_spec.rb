3473c734188cb837fb4aa4f428b2bc73bd6a1c47
Maxwell Baird <bairdm71@gmail.com>
2020-03-25 09:29:14 -0600

    created a test to check good path of create review

---
+++ spec/features/reviews/user_can_create_review_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor", type: :feature do
+  it "I can create a shelter review at '/shelters/:id'" do
+
+    shelter = Shelter.create(name:    "Broomfield Pet Shelter",
+                             address: "1111 fake st.",
+                             city:    "Broomfield",
+                             state:   "CO",
+                             zip:     "80020")
+
+    click_link "Create Review"
+
+    expect(page).to have_current_path("/reviews/new")
+
+    fill_in :title, with: "Great Place"
+    fill_in :rating, with: 5
+    fill_in :content, with: "Love my new fluff ball"
+    fill_in :photo, with: 'http://pngimg.com/uploads/dog/dog_PNG50348.png'
+    click_on "Create Review"
+    expect(page).to have_current_path("/shelters/#{shelter.id}")
+
+    within("#review-1") do
+      expect(page).to have_content("Great Place")
+      expect(page).to have_content(5)
+      expect(page).to have_content("Love my new fluff ball")
+      expect(page).to have_xpath('http://pngimg.com/uploads/dog/dog_PNG50348.png')
+    end
+  end
+end
