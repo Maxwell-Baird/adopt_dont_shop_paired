7486835a10b98a909bdf28c0e7bf05d2e600634b
Maxwell Baird <bairdm71@gmail.com>
2020-03-28 17:58:53 -0600

    change destory to be more general

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -13,16 +13,22 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    pet = Pet.find(params[:pet_id])
-    @favorites = Favorites.new(session[:favorites])
-    @favorites.remove_pet(pet.id)
-    session[:favorites] = @favorites.contents
-    flash[:notice] = "#{pet.name} at #{pet.shelter.name} has been removed from favorites"
+    if params[:pet_id] == nil
+      session[:favorites] = []
+    else
+      pet = Pet.find(params[:pet_id])
+      @favorites = Favorites.new(session[:favorites])
+      @favorites.remove_pet(pet.id)
+      session[:favorites] = @favorites.contents
+      flash[:notice] = "#{pet.name} at #{pet.shelter.name} has been removed from favorites"
+      
+    end
     redirect_back fallback_location: @post
   end
 
   def destroy_all
-    session[:favorites] = []
+
+
     redirect_to "/favorites"
   end
 end
