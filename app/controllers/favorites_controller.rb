edcde3e08a336d4398fe97a274e94722c1eaed62
Maxwell Baird <bairdm71@gmail.com>
2020-03-28 15:41:16 -0600

    added a link to remove pet from index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -18,7 +18,7 @@ class FavoritesController < ApplicationController
     @favorites.remove_pet(pet.id)
     session[:favorites] = @favorites.contents
     flash[:notice] = "#{pet.name} at #{pet.shelter.name} has been removed from favorites"
-    redirect_to "/pets/#{pet.id}"
+    redirect_back fallback_location: @post
   end
 
   def destroy_all
