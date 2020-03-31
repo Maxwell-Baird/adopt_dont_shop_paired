61aa0267d06bc2eea9c5eba90b2dedc4bdd214fa
Alex Latham <alexlthm@gmail.com>
2020-03-31 16:17:14 -0600

    Add test for list of pets with applications on them.

--- README.md
+++ README.md
@@ -157,7 +157,7 @@ I am taken to the favorites index page
 ```
 
 ```
-[ ] done
+[X] done
 
 User Story 12, Can't Favorite a Pet More Than Once
 
@@ -174,7 +174,7 @@ And I also see that my favorites indicator has decremented by 1
 ```
 
 ```
-[ ] done
+[X] done
 
 User Story 13, Remove a Favorite from Favorites Page
 
@@ -189,7 +189,7 @@ And I also see that the favorites indicator has decremented by 1
 ```
 
 ```
-[ ] done
+[X] done
 
 User Story 14, No Favorites Page
 
@@ -200,7 +200,7 @@ I see text saying that I have no favorited pets
 ```
 
 ```
-[ ] done
+[X] done
 
 User Story 15, Remove all Favorite from Favorites Page
 
@@ -243,7 +243,7 @@ And I'm taken back to my favorites page where I no longer see the pets for which
 ```
 
 ```
-[ ] done
+[X] done
 
 User Story 17, Incomplete application for a Pet
 
--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -1,100 +1,87 @@
 require 'rails_helper'
 
 RSpec.describe "As a visitor", type: :feature do
-  it "I can see pets that have been favorited" do
-
-    shelter = Shelter.create(name:    "Dumb Friends League",
+  before(:each) do
+    @shelter = Shelter.create(name:    "Dumb Friends League",
                              address: "2080 S. Quebec St.",
                              city:    "Denver",
                              state:   "CO",
                              zip:     "80231")
 
-    pet_1 = shelter.pets.create(image:        "https://i.imgur.com/9AyaA0q.jpg",
+    @pet1 = @shelter.pets.create(image:        "https://i.imgur.com/9AyaA0q.jpg",
                                 name:         "Kona",
                                 description:  "Kona greets everyone with the biggest smile! He's always happy and is so easy to fall in love with. He seems to love everyone he meets, but can get a little overly excited some times and may knock little kids down. He is reportedly housebroken and does well when left alone in the home. He would benefit from daily walks and lots of playtime!",
                                 approx_age:   6,
                                 sex:          "male",
                                 status:       "adoptable")
-    pet_2 = shelter.pets.create(image:        "http://www.pngall.com/wp-content/uploads/4/Golden-Retriever-Puppy-PNG.png",
+    @pet2 = @shelter.pets.create(image:        "http://www.pngall.com/wp-content/uploads/4/Golden-Retriever-Puppy-PNG.png",
                                 name:         "Max",
                                 description:  "Max likes to eat all different types of food",
                                 approx_age:   3,
                                 sex:          "male",
                                 status:       "adoptable")
-    visit "/pets/#{pet_1.id}"
+    visit "/pets/#{@pet1.id}"
     click_link "Favorite Pet"
 
-    visit "/pets/#{pet_2.id}"
+    visit "/pets/#{@pet2.id}"
     click_link "Favorite Pet"
     visit "/favorites"
+  end
 
-    within("#pet-#{pet_1.id}") do
-      expect(page).to have_content(pet_1.name)
-      expect(page).to have_css("img[src*='#{pet_1.image}']")
+  it "I can see pets that have been favorited" do
+    within("#pet-#{@pet1.id}") do
+      expect(page).to have_content(@pet1.name)
+      expect(page).to have_css("img[src*='#{@pet1.image}']")
     end
 
-    within("#pet-#{pet_2.id}") do
-      expect(page).to have_content(pet_2.name)
-      expect(page).to have_css("img[src*='#{pet_2.image}']")
+    within("#pet-#{@pet2.id}") do
+      expect(page).to have_content(@pet2.name)
+      expect(page).to have_css("img[src*='#{@pet2.image}']")
     end
   end
 
   it "I can remove a pet that has been favorited" do
-
-    shelter = Shelter.create(name:    "Dumb Friends League",
-                             address: "2080 S. Quebec St.",
-                             city:    "Denver",
-                             state:   "CO",
-                             zip:     "80231")
-
-    pet_1 = shelter.pets.create(image:        "https://i.imgur.com/9AyaA0q.jpg",
-                                name:         "Kona",
-                                description:  "Kona greets everyone with the biggest smile! He's always happy and is so easy to fall in love with. He seems to love everyone he meets, but can get a little overly excited some times and may knock little kids down. He is reportedly housebroken and does well when left alone in the home. He would benefit from daily walks and lots of playtime!",
-                                approx_age:   6,
-                                sex:          "male",
-                                status:       "adoptable")
-    pet_2 = shelter.pets.create(image:        "http://www.pngall.com/wp-content/uploads/4/Golden-Retriever-Puppy-PNG.png",
-                                name:         "Max",
-                                description:  "Max likes to eat all different types of food",
-                                approx_age:   3,
-                                sex:          "male",
-                                status:       "adoptable")
-    visit "/pets/#{pet_1.id}"
-    click_link "Favorite Pet"
-
-    visit "/pets/#{pet_2.id}"
-    click_link "Favorite Pet"
-    visit "/favorites"
-
-    within("#pet-#{pet_1.id}") do
-      expect(page).to have_content(pet_1.name)
-      expect(page).to have_css("img[src*='#{pet_1.image}']")
-      expect(page).to have_content("Remove from favorites")
-    end
-    within("#pet-#{pet_2.id}") do
-      expect(page).to have_content(pet_2.name)
-      expect(page).to have_css("img[src*='#{pet_2.image}']")
-      expect(page).to have_content("Remove from favorites")
-    end
-    within("#pet-#{pet_1.id}") do
+    within("#pet-#{@pet1.id}") do
       click_link "Remove from favorites"
     end
 
     within("main") do
-      expect(page).to have_no_content(pet_1.name)
-      expect(page).to have_no_css("img[src*='#{pet_1.image}']")
+      expect(page).to have_no_content(@pet1.name)
+      expect(page).to have_no_css("img[src*='#{@pet1.image}']")
     end
 
-    within("#pet-#{pet_2.id}") do
-      expect(page).to have_content(pet_2.name)
-      expect(page).to have_css("img[src*='#{pet_2.image}']")
+    within("#pet-#{@pet2.id}") do
+      expect(page).to have_content(@pet2.name)
+      expect(page).to have_css("img[src*='#{@pet2.image}']")
       expect(page).to have_content("Remove from favorites")
     end
   end
 
-  it "will display only a statement" do
-
-    visit "/favorites"
+  it "will display a statement when there are no favorite pets" do
+    click_link "Remove All Favorited Pets"
     expect(page).to have_content("You have no favorited pets")
+  end
+
+  it "I can see a list of pets that have applications on them" do
+    click_link "Adopt Pets"
+    select @pet1.name, from: :pets
+    select @pet2.name, from: :pets
+    fill_in :name, with: @application_info[:name]
+    fill_in :address, with: @application_info[:address]
+    fill_in :city, with: @application_info[:city]
+    fill_in :state, with: @application_info[:state]
+    fill_in :zip, with: @application_info[:zip]
+    fill_in :phone, with: @application_info[:phone]
+    fill_in :description, with: @application_info[:description]
+    click_button "Submit Application"
+
+    within('#pets-with-application') do
+      expect(page).to have_content(@pet1.name)
+      expect(page).to have_content(@pet2.name)
+
+      click_link @pet2.name
+
+      expect(page).to have_current_path("/pets/#{pet2.id}")
+    end
   end
 end
