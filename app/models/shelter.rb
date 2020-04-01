f7c851354044c5afd88eb80f4e27005ca37e3c67
Alex Latham <alexlthm@gmail.com>
2020-03-31 22:58:42 -0600

    Add ability to show statistics on shelter show page

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -3,4 +3,12 @@ class Shelter < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates_presence_of :name, :address, :city, :state, :zip
+
+  def average_rating
+    reviews.empty? ? 0 : reviews.sum { |review| review.rating }.fdiv(reviews.length)
+  end
+
+  def application_count
+    pets.empty? ? 0 : pets.joins(:applications).length
+  end
 end
